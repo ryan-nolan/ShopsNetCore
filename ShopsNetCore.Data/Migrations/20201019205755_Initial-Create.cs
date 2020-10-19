2f3b2a936d9f713fb46756f29ad20706c46c04f0
Ryan <nolanr6969@gmail.com>
2020-10-19 22:02:37 +0100

    Added Initial-Create Migration for Sql Data store.

---
+++ ShopsNetCore.Data/Migrations/20201019205755_Initial-Create.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopsNetCore.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Description = table.Column<string>(maxLength: 80, nullable: false),
+                    Location = table.Column<string>(maxLength: 40, nullable: false),
+                    ShopType = table.Column<int>(nullable: false),
+                    OpeningTime = table.Column<DateTime>(nullable: false),
+                    ClosingTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shops");
+        }
+    }
+}
